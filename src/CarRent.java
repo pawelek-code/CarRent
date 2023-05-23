6150e60fcb4948f233e12ecc798e49e95b4c4e20
Pawel <pawel.wylezek@student.wsb.edu.pl>
2023-05-24 01:38:27 +0200

    Commit add and del car

--- src/CarRent.java
+++ src/CarRent.java
@@ -7,21 +7,41 @@ public class CarRent {
     CarRent() {
         vehicles = new ArrayList<>();
     }
+
+    public List<Vehicle> getVehicles() {
+        return vehicles;
+    }
   public void addCar(Vehicle vehicle)
   {
       vehicles.add(vehicle);
   }
   public void rentaCar(Vehicle vehicle)
   {
+      if (vehicle.availability == true) {
       vehicle.setAvailability(false);
+      System.out.println("The car has been rented");
+     }
+  else{
+      System.out.println("The car is not avaible");
   }
-  public void deleteCar(Vehicle vehicle)
-  {
-      vehicle.brand=null;
-      vehicle.model=null;
-      vehicle.plates=null;
-      vehicle.prodYear=0;
-      vehicle.power=0;
-      vehicle.kmLimit=0;
   }
+    public void returnRentedCar(Vehicle vehicle){
+
+        vehicle.setAvailability(true);
+    }
+  public void deleteCar(Vehicle vehicle){
+    vehicle=null;
+  }
+    public void printAvaibleCars(){}
+    public void printAllCarsFrom() {
+        for (Vehicle vehicle : vehicles)
+            System.out.println(vehicle.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "CarRent{" +
+                "vehicles=" + vehicles +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,130 @@
-import java.sql.SQLOutput;
+import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-        PetrolCar honda = new PetrolCar("Honda", "civic","SPI11424",2004,90,2000 ,1400,60);
+        PetrolCar honda = new PetrolCar("Honda", "civic", "SPI11424", 2005, 90, 2000, 1400, 60);
         System.out.println(honda);
-        PetrolCar honda2 = new PetrolCar("Honda", "civic","SPI11424",2004,90,2000 ,1400,60);
+        PetrolCar honda2 = new PetrolCar("Honda", "civic", "SPI11424", 2005, 90, 2000, 1400, 60);
 
         System.out.println(honda.equals(honda2));
-            CarRent januszex = new CarRent();
-            PetrolCar ford = new PetrolCar("Ford", "Focus","SPI21808",2012,150,2000,1600,30 );
+        CarRent januszex = new CarRent();
+        PetrolCar ford = new PetrolCar("Ford", "Focus", "SPI21808", 2012, 150, 2000, 1600, 30);
         System.out.println(ford);
         januszex.addCar(ford);
         januszex.addCar(honda);
-    }
+        Scanner scanner = new Scanner(System.in);
+        boolean running=true;
+        while (running == true){
+        System.out.println("What do you want to do?\n" +
+                "1. Add new petrol car\n" +
+                "2.  Add new electric car\n" +
+                "3.  Add new Motorbike\n" +
+                "4. Close the program");
+        int tmp = scanner.nextInt();
 
+
+            switch (tmp) {
+                case 1:
+                    PetrolCar petrolCar = new PetrolCar();
+                    System.out.println("add engine capacity");
+                    int tmpEngCap = scanner.nextInt();
+                    petrolCar.setEngCapacity(tmpEngCap);
+
+                    System.out.println("choose brand");
+                    String branTemp = scanner.nextLine();
+                    scanner.nextLine();
+                    petrolCar.setBrand(branTemp);
+
+                    System.out.println("choose model");
+                    String modeltemp = scanner.nextLine();
+
+                    petrolCar.setModel(modeltemp);
+
+                    System.out.println("add Production year");
+                    int prodTemp = scanner.nextInt();
+
+                    petrolCar.setProdYear(prodTemp);
+
+                    System.out.println(" power");
+                    int powTemp = scanner.nextInt();
+
+                    petrolCar.setPower(powTemp);
+
+                    System.out.println("plates number");
+                    String plaTemp = scanner.nextLine();
+                    scanner.nextLine();
+                    petrolCar.setPlates(plaTemp);
+
+                    System.out.println("choose kmlimit");
+                    int limTemp = scanner.nextInt();
+
+                    petrolCar.setKmLimit(limTemp);
+
+                    System.out.println("Fuel percentage");
+                    int fuelTemp = scanner.nextInt();
+
+                    petrolCar.setFuelPrecent(fuelTemp);
+                    januszex.addCar(petrolCar);
+                    break;
+                case 2:
+                    ElectricCar electricCar = new ElectricCar();
+                    System.out.println("battery lvl");
+                    int tmpBatlvl = scanner.nextInt();
+                    electricCar.setBatteryLvl(tmpBatlvl);
+                    System.out.println("choose brand");
+                    String bran1Temp = scanner.nextLine();
+                    electricCar.setBrand(bran1Temp);
+                    System.out.println("choose model");
+                    String model1temp = scanner.nextLine();
+                    electricCar.setModel(model1temp);
+                    System.out.println("add Production year");
+                    int prod1Temp = scanner.nextInt();
+                    electricCar.setProdYear(prod1Temp);
+                    System.out.println(" power");
+                    int pow1Temp = scanner.nextInt();
+                    electricCar.setPower(pow1Temp);
+                    System.out.println("plates number");
+                    String pla1Temp = scanner.nextLine();
+                    electricCar.setPlates(pla1Temp);
+                    System.out.println("choose kmlimit");
+                    int lim1Temp = scanner.nextInt();
+                    electricCar.setKmLimit(lim1Temp);
+                    januszex.addCar(electricCar);
+                    break;
+                case 3:
+                    Motorbike motorbike = new Motorbike();
+                    System.out.println("choose brand");
+                    String bran2Temp = scanner.nextLine();
+                    motorbike.setBrand(bran2Temp);
+                    System.out.println("choose model");
+                    String model2temp = scanner.nextLine();
+                    motorbike.setModel(model2temp);
+                    System.out.println("add Production year");
+                    int prod2Temp = scanner.nextInt();
+                    motorbike.setProdYear(prod2Temp);
+                    System.out.println(" power");
+                    int pow2Temp = scanner.nextInt();
+                    motorbike.setPower(pow2Temp);
+                    System.out.println("plates number");
+                    String pla2Temp = scanner.nextLine();
+                    motorbike.setPlates(pla2Temp);
+                    System.out.println("choose kmlimit");
+                    int lim2Temp = scanner.nextInt();
+                    motorbike.setKmLimit(lim2Temp);
+                    System.out.println("Motor type");
+                    String typTemp = scanner.nextLine();
+                    motorbike.setType(typTemp);
+                    januszex.addCar(motorbike);
+                    break;
+
+                case 4:
+                    running=false;
+                    break;
+                default:
+                    System.out.println("wrong number");
+            }
+        }
+    }
 
     }
